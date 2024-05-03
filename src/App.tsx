d562c1bed5a796301f7535a17603eb3cc9be938f
zakaria <zakariabaouali@outlook.com>
2024-05-03 15:16:50 +0100

    update app folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import { Register } from "./components/Auth";
+import { Register, SignUp } from "./components/Auth";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <Register />
+      <Router>
+        <Routes>
+          <Route path="register" element={<Register />} />
+          <Route path="signup/*" element={<SignUp />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
